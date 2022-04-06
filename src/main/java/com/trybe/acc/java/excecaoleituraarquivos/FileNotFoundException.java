2fb9b6801eca9f40d7ad1836e84b923e26d4143b
ricardo-sousa <rwmsousa@gmail.com>
2022-04-05 21:18:36 -0300

    bug: try resolve exercise

---
+++ src/main/java/com/trybe/acc/java/excecaoleituraarquivos/FileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.trybe.acc.java.excecaoleituraarquivos;
+
+public class FileNotFoundException extends Exception {
+
+  public FileNotFoundException() {
+    super("Arquivo não existe");
+  }
+}
