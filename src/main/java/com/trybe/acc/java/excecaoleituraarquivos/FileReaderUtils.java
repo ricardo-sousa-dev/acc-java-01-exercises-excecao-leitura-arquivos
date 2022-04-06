afdc33a58c86599d69294b9fc4abcdabee4b2a8e
ricardo-sousa <rwmsousa@gmail.com>
2022-04-06 09:11:15 -0300

    feat: resolution exercise

--- src/main/java/com/trybe/acc/java/excecaoleituraarquivos/FileReaderUtils.java
+++ src/main/java/com/trybe/acc/java/excecaoleituraarquivos/FileReaderUtils.java
@@ -9,80 +9,28 @@ import java.io.IOException;
 public class FileReaderUtils {
 
   /** Retorna a primeira linha de um arquivo. */
-  public String getFirstLine() throws FileNotFoundException {
-    BufferedReader reader = new BufferedReader(new FileReader("arquivo_nao_existente.txt"));
-    try {
+  public String getFirstLine() {
+    BufferedReader reader = null;
+    String file = "";
 
-      return reader.lines().findFirst().get();
+    try {
+      reader = new BufferedReader(new FileReader("arquivo_nao_existente.txt"));
+      file = reader.lines().findFirst().get();
+
+    } catch (FileNotFoundException e) {
+      return "Arquivo não existe";
 
     } finally {
       try {
-        reader.close();
+        if (reader != null) {
+          reader.close();
+        }
+
       } catch (IOException e) {
         System.out.println(e.getMessage());
       }
     }
-  }
 
-  // public class FileNotFoundException extends Exception {
-
-  // public FileNotFoundException() {
-  // super("Arquivo não existe");
-  // }
-  // }
-
-  public static void main(String[] args) {
-    FileReaderUtils fileReaderUtils = new FileReaderUtils();
-    try {
-      System.out.println(fileReaderUtils.getFirstLine());
-    } catch (FileNotFoundException e) {
-      System.out.println("Arquivo não existe");
-    }
+    return file;
   }
 }
-
-// public class FileReaderUtils {
-
-// /** Retorna a primeira linha de um arquivo. */
-// public String getFirstLine() throws FileNotFoundException {
-// BufferedReader reader = new BufferedReader(new
-// FileReader("arquivo_nao_existente.txt"));
-// try {
-// return reader.lines().findFirst().get();
-
-// } finally {
-// try {
-// if (reader != null) {
-// reader.close();
-// }
-// } catch (FileNotFoundException e) {
-// throw new FileNotFoundException();
-// } catch (IOException e) {
-// System.out.println(e.getMessage());
-// }
-// }
-// }
-// }
-
-// public class FileReaderUtils {
-
-// /** Retorna a primeira linha de um arquivo. */
-// public String getFirstLine() throws FileNotFoundException {
-// BufferedReader reader = new BufferedReader(new
-// FileReader("arquivo_nao_existente.txt"));
-// try {
-// return reader.lines().findFirst().get();
-
-// } catch (Throwable e) {
-// System.out.println("Arquivo não existe");
-// } finally {
-// if (reader != null) {
-// try {
-// reader.close();
-// } catch (IOException e) {
-// e.printStackTrace();
-// }
-// }
-// }
-// return null;
-// }
